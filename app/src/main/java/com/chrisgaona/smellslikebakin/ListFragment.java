0724cd57b15c859bcf2ddc8966345803990ee038
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-08 09:15:05 -0800

    Wires up ListAdapter with ListFragment

--- app/src/main/java/com/chrisgaona/smellslikebakin/ListFragment.java
+++ app/src/main/java/com/chrisgaona/smellslikebakin/ListFragment.java
@@ -3,6 +3,8 @@ package com.chrisgaona.smellslikebakin;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,14 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         // boolean says whether to attach this to the viewgroup...error will be thrown if not used because it will be added twice
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        // attach layout manager to recycler view
+        recyclerView.setLayoutManager(layoutManager);
+
         return view;
     }
 }
