757c6c217807d01ea3e7bd4b31780f9cb88dfafd
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-09 09:45:39 -0800

    Initializes a fragment with data

--- app/src/main/java/com/chrisgaona/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/chrisgaona/smellslikebakin/MainActivity.java
@@ -8,13 +8,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnRecipeSelectedInterface {
 
+    public static final String LIST_FRAGMENT = "LIST_FRAGMENT";
+    public static final String VIEWPAGER_FRAGMENT = "VIEWPAGER_FRAGMENT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // this following line makes sure we have access to variables and other stuff we need from our mainactivity
-        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
+        // uses keys now for fragments LIST_FRAGMENT
+        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentByTag(LIST_FRAGMENT);
 
         // if we didn't have this savedInstanceState check...
         // when we rotate the app it will create a new fragment over the old fragment every time
@@ -22,20 +26,26 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnRe
             ListFragment fragment = new ListFragment();
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeHolder, fragment);
+            // uses keys now for fragments LIST_FRAGMENT
+            fragmentTransaction.add(R.id.placeHolder, fragment, LIST_FRAGMENT);
             fragmentTransaction.commit();
         }
     }
 
     @Override
     public void onListRecipeSelected(int index) {
-        Toast.makeText(MainActivity.this, Recipes.names[index], Toast.LENGTH_LONG).show();
-
         ViewPagerFragment fragment = new ViewPagerFragment();
+
+        // creates bundle
+        Bundle bundle = new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_RECIPE_INDEX, index);
+        fragment.setArguments(bundle);
+
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         // use replace to change to another fragment
-        fragmentTransaction.replace(R.id.placeHolder, fragment);
+        // uses keys now for fragments VIEWPAGER_FRAGMENT
+        fragmentTransaction.replace(R.id.placeHolder, fragment, VIEWPAGER_FRAGMENT);
         // use addToBackStack method to stack fragment on top of each other
         // so the back button can be used to go to the last fragment on the stack
         // pass in null because we will only need to go back one transaction at a time
--- app/src/main/java/com/chrisgaona/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/chrisgaona/smellslikebakin/ViewPagerFragment.java
@@ -6,16 +6,31 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by chrisgaona on 11/9/17.
  */
 
 public class ViewPagerFragment extends Fragment {
+
+    public static final String KEY_RECIPE_INDEX = "KEY_RECIPE_INDEX";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        // gets argument from bundle created on MainActivity
+        int index = getArguments().getInt(KEY_RECIPE_INDEX);
+        getActivity().setTitle(Recipes.names[index]);
+
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // resets the title name on fragment stop
+        getActivity().setTitle(getResources().getString(R.string.app_name));
     }
 }
