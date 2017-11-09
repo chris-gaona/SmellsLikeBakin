a4ae1978ca893b1e89891ef005da1e99409f1fe0
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-09 08:52:37 -0800

    Works with Fragment lifecycles; keeps track of fragment instance state

--- app/src/main/java/com/chrisgaona/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/chrisgaona/smellslikebakin/MainActivity.java
@@ -13,11 +13,18 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnRe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment fragment = new ListFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.placeHolder, fragment);
-        fragmentTransaction.commit();
+        // this following line makes sure we have access to variables and other stuff we need from our mainactivity
+        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
+
+        // if we didn't have this savedInstanceState check...
+        // when we rotate the app it will create a new fragment over the old fragment every time
+        if (savedFragment == null) {
+            ListFragment fragment = new ListFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.placeHolder, fragment);
+            fragmentTransaction.commit();
+        }
     }
 
     @Override
