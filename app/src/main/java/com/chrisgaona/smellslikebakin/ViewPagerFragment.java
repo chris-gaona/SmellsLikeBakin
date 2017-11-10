d5d9b807dc339e8f8ff6cdea8eb58d5a3bc072ea
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-10 09:06:08 -0800

    Wires up the tabs to the ViewPager

--- app/src/main/java/com/chrisgaona/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/chrisgaona/smellslikebakin/ViewPagerFragment.java
@@ -2,6 +2,7 @@ package com.chrisgaona.smellslikebakin;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -39,11 +40,20 @@ public class ViewPagerFragment extends Fragment {
             }
 
             @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
+
+            @Override
             public int getCount() {
                 // viewpager hosts 2 fragment (ingredients or directions)
                 return 2;
             }
         });
+
+        // wires up tabLayout with viewPager
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
